bf0f0d06682c4c4d9a0dc70738d25339cd1bf78e
David Mitjana Castro <d.mitjana@gmail.com>
2019-10-29 17:58:41 +0100

    done until iteration 5

--- js/index.js
+++ js/index.js
@@ -1,9 +1,10 @@
-function deleteItem(event){
-
+function deleteItem(event, index){
+  var productsWrapper = document.getElementsByClassName('product');
+  var products = [...productsWrapper];
+  products[index].parentNode.removeChild(products[index])
 }
 
 function getPriceByProduct(itemNode){
-
 }
 
 function updatePriceByProduct(productPrice, index){
@@ -11,6 +12,48 @@ function updatePriceByProduct(productPrice, index){
 }
 
 function getTotalPrice() {
+  var productsWrapper = document.getElementsByClassName('product');
+  var products = [...productsWrapper];
+  //console.log(products[0])
+  var productsPrices = [];
+  var productsQuantities = [];
+  var totPricesArr = [];
+
+  products.forEach( function() {
+    var pricesSpans = document.getElementsByClassName('cost');
+    var prices = [...pricesSpans];
+
+    prices.forEach( function(price, i) {
+      productsPrices[i] = price.innerHTML.slice(1);
+    });
+    console.log(productsPrices);
+
+    var quantitySpans = document.getElementsByClassName('quantity');
+    var quantities = [...quantitySpans];
+    console.log(quantities);
+    quantities.forEach( function(quantity, i) {
+      productsQuantities[i] = quantity.value;
+    });
+
+    var totalPriceDivs = document.getElementsByClassName('product-total-price');
+    var totalPriceDiv = [...totalPriceDivs];
+
+    totalPriceDiv.forEach( function (productPrice, i) {
+      var productTotPrice = (productsPrices[i] * productsQuantities[i]).toFixed(2);
+      totPricesArr[i] = parseFloat(productTotPrice);
+      productPrice.innerHTML = `$${productTotPrice}`;
+    });
+  });
+
+  var totalPrice = totPricesArr.reduce( function(accumulator, currentPrice) {
+    accumulator += currentPrice;
+    return accumulator;
+  }, 0);
+
+  var totalPriceSpan = document.getElementById('total-price');
+  totalPriceSpan.innerHTML = `$${parseFloat(totalPrice)}`;
+
+  console.log(totalPrice);
 
 }
 
@@ -27,15 +70,22 @@ function createQuantityNode(){
 }
 
 function createItemNode(dataType, itemData){
-
+  let newProduct = document.createElement('div');
+  newProduct.classList.add('product');
 }
 
 function createNewItemRow(itemName, itemUnitPrice){
 
+
+
+
 }
 
 function createNewItem(){
+  let productName = document.getElementById('new-product-name');
+  let productPrice= document.getElementById('new-product-price');
 
+  createNewItemRow(productName.value,productPrice.value);
 }
 
 window.onload = function(){
@@ -43,10 +93,15 @@ window.onload = function(){
   var createItemButton = document.getElementById('new-item-create');
   var deleteButtons = document.getElementsByClassName('btn-delete');
 
+  var deleteArray = [...deleteButtons];
+
   calculatePriceButton.onclick = getTotalPrice;
   createItemButton.onclick = createNewItem;
 
-  for(var i = 0; i<deleteButtons.length ; i++){
-    deleteButtons[i].onclick = deleteItem;
-  }
+  deleteArray.forEach(function(button, index){
+    button.addEventListener('click', function(){
+    console.log(event);
+    deleteItem(event, index)
+    })
+  }) ;
 };
